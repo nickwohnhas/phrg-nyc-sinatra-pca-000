7b0997b946d175c11862a416c8e4d4386f6d7076
Garett Arrowood <garettarrowood@gmail.com>
2018-07-01 14:34:00 -0400

    Add pending rubocop spec

---
+++ spec/lint/rubocop_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "rubocop analysis" do
+  # Remove the "x" from "xit" to enable this lint test.
+  xit "has no offenses" do
+    expect(`rubocop`).to match(/no\ offenses\ detected/)
+  end
+end
